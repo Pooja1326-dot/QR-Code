8f743f3aa4bcc19d43303ed60237f32d1905a30e
Pooja1326-dot <pujuuahire1326@gmail.com>
2024-06-30 11:56:00 +0530

    added

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function generateQR() {
         QRimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         imgBox.classList.add("show-img");
     }
-   else{
-    qrText.classList.add(`error`);
-    setTimeout(()=>{
-       qrText.classList.remove(`error`);
-    },1000);
-   }
+    else {
+        qrText.classList.add(`error`);
+        setTimeout(() => {
+            qrText.classList.remove(`error`);
+        }, 1000);
+    }
 }
